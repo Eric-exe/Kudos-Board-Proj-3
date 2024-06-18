83901accff329c3a483d62f0e924ad0418aec874
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2024-06-17 20:14:10 -0700

    Added category buttons

--- client/src/index/App.jsx
+++ client/src/index/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import BrowseBar from "./BrowseBar";
 import Board from "./Board";
 
 function App() {
+    const [category, setCategory] = useState('All');
+
     return (
         <>
         <div className="d-flex justify-content-between text-white bg-success px-3 py-2">
             <h2 className="m-0">Kudos Board</h2>
             <button type="button" className="btn btn-outline-light">Guest</button>
         </div>
-            <BrowseBar />
+            <BrowseBar 
+                category={[category, setCategory]}
+            />
             <Board />
         </>
     );
--- client/src/index/BrowseBar.jsx
+++ client/src/index/BrowseBar.jsx
@@ -1,23 +1,66 @@
-function BrowseBar() {
+import { useEffect } from "react";
+import propTypes from "prop-types";
+
+BrowseBar.propTypes = {
+    category: propTypes.array.isRequired, // 0 -> value, 1 -> function
+};
+
+function BrowseBar(props) {
+    // update the active button
+    useEffect(() => {
+        const buttons = document.querySelectorAll(".btn");
+        buttons.forEach((button) => {
+            if (button.value == props.category[0]) {
+                button.classList.add("active");
+            }
+            else {
+                button.classList.remove("active");
+            }
+        });
+    }, [props.category[0]]);
+
+    // handle the button click event
+    const handleCategoryClick = (event) => {
+        props.category[1](event.target.value);
+    };
+
     return (
         <>
-            <div className='row justify-content-center'>
-                <div className='col-6 pt-4 pb-2'>
-                    <input className="form-control bg-light border border-success" type="text" placeholder="Search boards..."></input>
+            <div className="row justify-content-center">
+                <div className="col-7 pt-4">
+                    <input
+                        className="form-control bg-light border border-success mb-1"
+                        type="text"
+                        placeholder="Search boards..."
+                    ></input>
                 </div>
             </div>
 
-            <div className='row justify-content-center'>
-                <div className='d-flex col-6 justify-content-center overflow-scroll'>
-                    <button className='btn btn-outline-success mx-2'>All</button>
-                    <button className='btn btn-outline-success mx-2'>Recent</button>
-                    <button className='btn btn-outline-success mx-2'>Celebration</button>
-                    <button className='btn btn-outline-success mx-2'>Thank You</button>
-                    <button className='btn btn-outline-success mx-2'>Inspiration</button>
-                </div>
+            <div className="d-flex flex-wrap justify-content-center">
+                <button className="btn btn-outline-success mt-1 mx-1" value="All" onClick={handleCategoryClick}>
+                    All
+                </button>
+                <button className="btn btn-outline-success mt-1 mx-1" value="Recent" onClick={handleCategoryClick}>
+                    Recent
+                </button>
+                <button className="btn btn-outline-success mt-1 mx-1" value="Celebration" onClick={handleCategoryClick}>
+                    Celebration
+                </button>
+                <button className="btn btn-outline-success mt-1 mx-1" value="Thank You" onClick={handleCategoryClick}>
+                    Thank You
+                </button>
+                <button className="btn btn-outline-success mt-1 mx-1" value="Inspiration" onClick={handleCategoryClick}>
+                    Inspiration
+                </button>
+            </div>
+
+            <div className="d-flex flex-wrap justify-content-center">
+                <button className="btn btn-outline-success mt-1 mx-1" value="Create">
+                    Create New Board
+                </button>
             </div>
         </>
-    )
+    );
 }
 
-export default BrowseBar
+export default BrowseBar;
